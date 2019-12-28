538273321c6397d89917e177128364090f95da7c
smnadim21 <smnadim21@gmail.com>
2019-12-28 13:18:34 +0600

    by smnadim21

---
+++ push2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+//    cout << "You have entered " << argc
+//         << " arguments:" << "\n";
+//
+//    for (int i = 0; i < argc; ++i)
+//        cout << argv[i] << "\n";
+
+    cout << "git status" << endl;
+    system("git status");
+    cout << "git add ." << endl;
+    system("git add .");
+
+    string command = "git commit -m \"";
+    for (int i = 1; i < argc; ++i)\
+    {
+        command += argv[i];
+        command += " ";
+    }
+
+    command += " by smnadim21\"";
+
+    cout << command << endl;
+    system(command.c_str());
+
+
+    return 0;
+}
